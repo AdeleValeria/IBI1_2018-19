01760b8a1fc233085fe1fab6a973747be9e8c77d
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-20 09:45:06 +0800

    Add a bar graph

--- Practical5/DNA.py
+++ Practical5/DNA.py
@@ -10,3 +10,16 @@ colors = ['cyan', 'beige', 'orange', 'indigo', 'gray']
 plt.pie(sizes, labels=label, autopct='%1.1f%%', shadow=True, startangle=90, explode=explode, colors=colors)
 plt.axis('equal')
 plt.show()
+
+#bar graph
+import numpy as np
+import matplotlib.pyplot as plt
+N = 5
+x = (s.count("A"), s.count("G"), s.count("C"), s.count("T"), s.count("U"))
+ind = np.arange(N)
+width = 0.5
+p1 = plt.bar(ind, x, width)
+plt.ylabel('Total')
+plt.xticks(ind, ('A', 'G', 'C', 'T', 'U'))
+plt.yticks(np.arange(0, 20, 2))
+plt.show
