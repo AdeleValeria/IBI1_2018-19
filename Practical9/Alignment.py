22899963520336cab823c3dd7049a60eefd7ee3e
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-05-15 20:51:24 +0800

    Added normalized score and graphical alignment

--- Practical9/Alignment.py
+++ Practical9/Alignment.py
@@ -3,9 +3,10 @@
 Created on Tue Apr 16 22:36:05 2019
 
 
-@author: HP
+@author: Adele
 """
 
+#BLOSUM62 matrix
 def get_blosum62_dictionary():
     blosum_raw = """A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4 
 R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1  0 -1 -4 
@@ -32,51 +33,51 @@ Z -1  0  0  1 -3  3  4 -2  0 -3 -3  1 -1 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4
 X  0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2  0  0 -2 -1 -1 -1 -1 -1 -4 
 * -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4  1 """
     
+    #split each line in the matrix into row
     blosum_array1=[row.split(" ") for row in blosum_raw.split("\n")]   
+    #c refers to each aplhabet in the row
     blosum_array2= [[c for c in row if c!=""] for row in blosum_array1]
     
     blosum62 = {}
     blosum_entry_count = len(blosum_array2)
     for j in range(0, blosum_entry_count):
-        for k in range(1,blosum_entry_count): #skipping the first column which has the residue code still
-            #the residues codes are all in the first column
+        for k in range(1,blosum_entry_count):
             row = blosum_array2[j]
             key = row[0] + blosum_array2[k-1][0]
+            #the score
             value = int(row[k])
+            #match each pair with its value, make a dictionary
             blosum62[key] = value
     return blosum62
 
-task = input("Select task:\n 1. Human-mouse\n 2. Human-random\n 3. Mouse-random\n")
+#main menu
+task = input("Select task (input 1, 2 or 3):\n 1. Human-mouse\n 2. Human-random\n 3. Mouse-random\n")
 if task == '1':
 #HUMAN - MOUSE
-    s1 = input("File path of human sequence (please include .txt):\n")
-    with open(s1) as s1_open:
-        s1_read = s1_open.read()
-    s2 = input("File path of mouse sequence (please include .txt):\n")
-    with open(s2) as s2_open:
-        s2_read = s2_open.read()
+    #'with open' closes automatically
+    with open('Human.txt') as s1:
+        s1_read = s1.read()
+    with open('Mouse.txt') as s2:
+        s2_read = s2.read()
     
 elif task == '2':
 #HUMAN - RANDOM
-    s1 = input("File path of human sequence (include .txt):\n")
-    with open(s1) as s1_open:
-        s1_read = s1_open.read()
-    s2 = input("File path of random sequence (include .txt):\n")
-    with open(s1) as s2_open:
-        s2_read = s2_open.read()
+    with open('Human.txt') as s1:
+        s1_read = s1.read()
+    with open('Random.txt') as s2:
+        s2_read = s2.read()
 
 elif task == '3':
 #MOUSE - RANDOM
-     s1 = input("File path of mouse sequence (include .txt):\n")
-     with open(s1) as s1_open:
-        s1_read = s1_open.read()
-     s2 = input("File path of random sequence (include .txt):\n")
-     with open(s2) as s2_open:
-        s2_read = s2_open.read()
+     with open('Mouse.txt') as s1:
+        s1_read = s1.read()
+     with open('Random.txt') as s2:
+        s2_read = s2.read()
 
    
 score = 0
 match = 0
+align = []
 for i in range (len(s1_read)):
     x = s1_read[i] + s2_read[i]
     compare = get_blosum62_dictionary()
@@ -84,8 +85,21 @@ for i in range (len(s1_read)):
     
     if s1_read[i] == s2_read[i]:
         match += 1
+        align.append(s1_read[i])
+    elif compare[x]>0:
+        align.append('+')
+    else:
+        align.append('-')
+
+#Calculate normalized score
+normalized = score/len(s1_read)
+
+#calculate the percentage identity (how many amino acids are identical)        
 perc = (match*100)/len(s1_read)
+
 print('\nBLOSUM62 score:', score)
-print('Percentage identity :', '%.2f' % perc, '%')
-
-
+#%.2f means 2 numbers after decimal
+print('\nNormalized score:', '%.2f' % normalized)
+print('\nPercentage identity :', '%.2f' % perc, '%\n')
+#to join the list into a single string
+print(*align, sep='')
