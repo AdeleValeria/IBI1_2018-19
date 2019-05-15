aea46e6a3e3dcf2e8df97bb2973eeb2146bf12ca
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-05-15 17:17:59 +0800

    Added a protocol, some description and changed the result list

--- Practical8/GO.py
+++ Practical8/GO.py
@@ -9,33 +9,42 @@ import xml.dom.minidom
 import pandas as pd
 import re
 
-result = set()
+result = []
+#Find childnodes
 def Child(id_no, result):
     for i in collection:
         parents = i.getElementsByTagName("is_a")
         cnodes = i.getElementsByTagName("id")[0].childNodes[0].data
         for j in parents:
             if j.childNodes[0].data == id_no:
-                result.add(cnodes)
+                #store the cnodes in result
+                result.append(cnodes)
                 Child(cnodes, result)
                 
+#CREATE DOM TREE-------------------------------------------
 
+#Open the xml file
 DOMTree = xml.dom.minidom.parse("go_obo.xml")
 collection = DOMTree.documentElement.getElementsByTagName("term")
 
+#Columns for pandas dataframe to store the output
 df = pd.DataFrame(columns=['ID','Name','Definition','Childnodes'])
 
 for term in collection:
     desc = term.getElementsByTagName("defstr")[0].childNodes[0].data
+    #Check if text in <defstr> contains the word 'autophagosome'
     if re.search("autophagosome", desc):
         id_no = term.getElementsByTagName("id")[0].childNodes[0].data
         name = term.getElementsByTagName("name")[0].childNodes[0].data
         defn = term.getElementsByTagName("defstr")[0].childNodes[0].data
     
+        #Call the function Child
         Child(id_no, result)
+        #len(result) to count the childnodes
         dc = {"ID":[id_no], "Name":[name], "Definition":[defn], "Childnodes":[len(result)]}
         df = df.append(pd.DataFrame(data = dc))
         
+#Convert to excel
 df.to_excel(r'C:\GitKraken\IBI1_2018-19\Practical8\Autophagosome.xlsx', index=False)
         
         
