9ee697971582299fa98e869e174995b2b58bf620
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-05-16 23:47:06 +0800

    Improved code

--- Practical13/Practical13.py
+++ Practical13/Practical13.py
@@ -12,49 +12,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Convert the xml file to cps
-#def xml_to_cps():
-#    os.system("C:\GitKraken\IBI1_2018-19\Practical13\CopasiSE -i predator-prey.xml -s predator-prey.cps")
+def xml_to_cps():
+    os.system("C:\GitKraken\IBI1_2018-19\Practical13\CopasiSE -i predator-prey.xml -s predator-prey.cps")
     
-#    cpsTree = xml.dom.minidom.parse("predator-prey.cps")
-#    cpsCollection = cpsTree.documentElement
+    cpsTree = xml.dom.minidom.parse("predator-prey.cps")
+    cpsCollection = cpsTree.documentElement
     
-#    reportFile = xml.dom.minidom.parse("report_ref.xml")
-#    reportLine = reportFile.documentElement
+    reportFile = xml.dom.minidom.parse("report_ref.xml")
+    reportLine = reportFile.documentElement
     
-#    tasks = cpsCollection.getElementsByTagName("Task")
-#    for task in tasks:
-#        if task.getAttribute("name")=="Time-Course":
-#            task.setAttribute("scheduled","true")
-#            task.insertBefore(reportLine,task.childNodes[0])
-#            break
+    tasks = cpsCollection.getElementsByTagName("Task")
+    for task in tasks:
+        if task.getAttribute("name")=="Time-Course":
+            task.setAttribute("scheduled","true")
+            task.insertBefore(reportLine,task.childNodes[0])
+            break
         
     
-#    for taskDetails in task.childNodes:
-#        if taskDetails.nodeType ==1:
-#            if taskDetails.nodeName == "Problem":
-#                problem = taskDetails
+    for taskDetails in task.childNodes:
+        if taskDetails.nodeType ==1:
+            if taskDetails.nodeName == "Problem":
+                problem = taskDetails
                 
-#    for param in problem.childNodes:
-#        if param.nodeType ==1:
-#            if param.getAttribute("name")=="StepNumber":
-#                param.setAttribute("value","200")
-#            if param.getAttribute("name")=="StepSize":
-#                param.setAttribute("value","1")
-#            if param.getAttribute("name")=="Duration":
-#                param.setAttribute("value","200")
+    for param in problem.childNodes:
+        if param.nodeType ==1:
+            if param.getAttribute("name")=="StepNumber":
+                param.setAttribute("value","200")
+            if param.getAttribute("name")=="StepSize":
+                param.setAttribute("value","1")
+            if param.getAttribute("name")=="Duration":
+                param.setAttribute("value","200")
            
             
-#    report18 = xml.dom.minidom.parse("report18.xml")
-#    report = report18.documentElement
+    report18 = xml.dom.minidom.parse("report18.xml")
+    report = report18.documentElement
     
-#    listOfReports  =  cpsCollection.getElementsByTagName("ListOfReports")[0]
-#    listOfReports.appendChild(report)
+    listOfReports  =  cpsCollection.getElementsByTagName("ListOfReports")[0]
+    listOfReports.appendChild(report)
     
-#    cpsFile = open("predator-prey.cps","w", encoding = "utf-8")
-#    cpsTree.writexml(cpsFile)
-#    cpsFile.close()
+    cpsFile = open("predator-prey.cps","w", encoding = "utf-8")
+    cpsTree.writexml(cpsFile)
+    cpsFile.close()
     
-#xml_to_cps()
+xml_to_cps()
 
 #Reading and plotting simulation results
 model = pd.read_csv("modelResults.csv")
@@ -81,7 +81,6 @@ plt.plot(header[2], label = "Prey Population")
 plt.xlabel('Time')
 plt.ylabel('Population Size')
 plt.title('Time Course')
-plt.legend()
 #gcf : get current figure
 plt.gcf()
 plt.show
@@ -94,7 +93,6 @@ plt.plot(header[1], header[2])
 plt.xlabel('Predator Population')
 plt.ylabel('Prey Population')
 plt.title('Limit Cycle')
-plt.legend()
 #gcf : get current figure
 plt.gcf()
 plt.show
