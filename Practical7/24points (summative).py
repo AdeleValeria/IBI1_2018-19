ba57414e85be8b4a5fa80443ccada1f28f05d9d4
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-05-16 23:47:31 +0800

    Added some description

--- Practical7/24points (summative).py
+++ Practical7/24points (summative).py
@@ -9,23 +9,20 @@ from fractions import Fraction
 input_numbers = input("Type some numbers between 1 and 23: ").split(",")
 #Convert the integer
 input_numbers = list(map(int, input_numbers))
-#Check is the numbers provided by the user match the requirement or not
+#Check if the numbers provided by the user match the requirement or not
 valid_number = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,23)
 check = all(i in valid_number for i in input_numbers)
      
 count = 0
-solution = 0
 
 if check is True: 
-      
-    def dfs(n):
+#Create a function for recursion     
+    def cnt(n):
         global count
-        global solution
         count = count + 1
         
         if n == 1:
             if(float(input_numbers[0])==24):
-                solution = solution + 1
                 return 1
             else:
                 return 0
@@ -36,36 +33,40 @@ if check is True:
                 b = input_numbers[j]
                 input_numbers[j] = input_numbers[n-1]
                 
+                #Test all arithmetic combinations for two numbers
                 input_numbers[i] = a+b
-                if(dfs(n-1)):
+                if(cnt(n-1)):
                     return 1
                 
                 input_numbers[i] = b-a
-                if(dfs(n-1)):
+                if(cnt(n-1)):
                     return 1
                 
                 input_numbers[i] = a*b
-                if(dfs(n-1)):
+                if(cnt(n-1)):
                     return 1
                 
+                #divident is zero
                 if a:
-                    input_numbers[i] = Fraction(a,b)
-                    if(dfs(n-1)):
+                    input_numbers[i] = Fraction(b,a)
+                    if(cnt(n-1)):
                         return 1
                 if b:
                     input_numbers[i] = Fraction(a,b)
-                    if (dfs(n-1)):
+                    if (cnt(n-1)):
                         return 1
                     
                 input_numbers[i] = a
                 input_numbers[j] = b
         return 0
     
-    if (dfs(len(input_numbers))):
+    if (cnt(len(input_numbers))):
         print("Yes")
     else:
         print("No")
-    print('Recursion times:', count,', Solution:', solution)
+    #Count is the value for recursion times
+    print('Recursion times:', count)
 
+#if the input number is not between 1 and 23
 elif check is False:
     print("The numbers must be between 1 and 23")    
